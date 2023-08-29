b38e453cce9906ad5813e1cdc4f867c4ce056fa6
Bhartenjay Giri <138518156+Bhartenjay@users.noreply.github.com>
2023-08-29 20:15:16 +0530

    JS-101-learning

---
+++ Problem1.js
@@ -0,0 +1,5 @@
+//Print "Masai School" in the console followed by "A Transformation in Education" in next line
+let name="Masai School"
+console.log(name)
+let a="A Transformation in Education"
+console.log(a)
---
+++ Problem4.js
@@ -0,0 +1,9 @@
+console.log("🅽🅰🅼🅴 - 🅱🅷🅰🆁🆃🅴🅽🅰🅹🅰🆈 🅶🅸🆁🅸");
+console.log("🆂🅲🅷🅾🅾🅻 - 🅳.🅰.🆅");
+console.log("🅶🆁🅰🅳🅴 - 🅰");
+console.log("🆂🅴🅲🆃🅸🅾🅽 - 1");
+console.log("🆁🅾🅻🅻 🅽🅾. - 0007");
+console.log("🅼🅰🆁🅺🆂 :---------");
+console.log("🅿🅷🆈🆂🅸🅲🆂 - 82");
+console.log("🅼🅰🆃🅷🅴🅼🅰🆃🅸🅲🆂 -85");
+console.log("🅴🅽🅶🅻🅸🆂🅷 - 75");
